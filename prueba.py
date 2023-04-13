c78f3963fddecb30ac90cfe5cb1abac7acd87173
Nach91Mt <118282781+Nach91Mt@users.noreply.github.com>
2023-04-13 12:06:45 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -6,7 +6,7 @@ applicatio = Flask(__name__)
 @application.route('/')
 
 def index():
-    return 'hellow word
+    return 'hellow word'
     
 if __name__ =='__main__':
     application.run(host="0.0.0.0.",port=80,debug True)
